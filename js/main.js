836e0223273c00803f9edcbd8d6524c6157d521b
jvald043 <jvald043@gmail.com>
2018-05-13 21:58:35 -0400

    Updates

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 //JQuery
 $(function(){
  $('.header').height($(window).height());
+
+
+$(".navbar a, #smore").click(function(){
+  $("body,html").animate({
+   scrollTop:$("#" + $(this).data('value')).offset().top
+  },1000)
+});
+
+
 })
+
+
 
 
 //Main Javascript
